4f2e77a6c0d70d6a695632147486cb2a1f947f9e
laybson <laybson.cunha@ccc.ufcg.edu.br>
2018-07-28 17:25:11 -0300

    testing

--- quickstart.py
+++ quickstart.py
@@ -3,11 +3,19 @@
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
 
-      
+
 app = ClarifaiApp(api_key='692a9ce7cbc846f19facd68123c1f0a5')
-      
-      # get the general model
-      
-model = app.models.get('general-v1.3')
-image = ClImage(url='https://samples.clarifai.com/metro-north.jpg')
-print(model.predict([image]))
+
+model = app.models.create('impressionism', concepts=['monet'])
+
+winterVethuil1 = ClImage(url='https://www.wga.hu/detail/m/monet/05/1vethe2.jpg', concepts=['monet'])
+winterVethuil2 = ClImage(url='https://www.wga.hu/detail/m/monet/05/1vethe3.jpg', concepts=['monet'])
+
+app.inputs.bulk_create_images([winterVethuil1, winterVethuil2])
+
+# search by image url
+print app.inputs.search_by_image(url="https://www.wga.hu/detail/m/monet/05/1vethe2.jpg")
+
+model.train()
+
+model.predict([winterVethuil1])
