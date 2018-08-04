a1a2613b7c449a2a16e6634370806a1197510347
Dandara Sousa <dandara.sousa@ccc.ufcg.edu.br>
2018-08-04 20:57:21 -0300

    as funções

--- quickstart.py
+++ quickstart.py
@@ -2,20 +2,43 @@
 
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
+import csv
+
+app = ClarifaiApp(api_key='##putkey')
 
 
-app = ClarifaiApp(api_key='692a9ce7cbc846f19facd68123c1f0a5')
+def addImages(link, concept_list):
+    app.inputs.create_image_from_url(link, concepts=concept_list)
 
-model = app.models.create('impressionism', concepts=['monet'])
+def catchImages(data):
+    link = ""
+    concept_list = []
+    f = open("output.txt","w")
+    with open(data, 'rb') as csvfile:
+        datapainting = csv.reader(csvfile, delimiter=',')
+        n=0
+        for row in datapainting:
+            n += 1
+            if (n<=100 and n>1):
+                link = row[6]
+                concept_list.append(row[0])
+                concept_list.append(row[7])
+                addImages(link,concept_list)
+                concept_list = []
 
-winterVethuil1 = ClImage(url='https://www.wga.hu/detail/m/monet/05/1vethe2.jpg', concepts=['monet'])
-winterVethuil2 = ClImage(url='https://www.wga.hu/detail/m/monet/05/1vethe3.jpg', concepts=['monet'])
 
-app.inputs.bulk_create_images([winterVethuil1, winterVethuil2])
+data = "dados/paintings.csv"
+#catchImages(data)
+#model = app.models.create(model_id="puppy", concepts=["my puppy"]) modify to our context
+model = app.models.get('impressionism')
+
+#winterVethuil1 = ClImage(url='https://www.wga.hu/detail/m/monet/05/1vethe2.jpg', concepts=['monet']) put another
+
+#addImages(winterVethuil1) 
 
 # search by image url
-print app.inputs.search_by_image(url="https://www.wga.hu/detail/m/monet/05/1vethe2.jpg")
+#print app.inputs.search_by_image(url="https://www.wga.hu/detail/m/monet/05/1vethe2.jpg")
 
-model.train()
+#model.train()
 
-model.predict([winterVethuil1])
+#print(model.predict([winterVethuil1]))
